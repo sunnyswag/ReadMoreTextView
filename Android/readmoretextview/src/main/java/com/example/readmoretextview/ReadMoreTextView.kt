e9e94bcdf4400d1f7a19ae8e1a5df3441911c76e
sunnyswag <hhq126152@gmail.com>
2022-08-29 22:28:24 +0800

    test the demo successfully

--- Android/readmoretextview/src/main/java/com/example/readmoretextview/ReadMoreTextView.kt
+++ Android/readmoretextview/src/main/java/com/example/readmoretextview/ReadMoreTextView.kt
@@ -1,7 +1,14 @@
 package com.example.readmoretextview
 
 import android.content.Context
+import android.graphics.Color
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatTextView
 
 // TODO: why need change TextView to AppCompatTextView
@@ -14,6 +21,28 @@ class ReadMoreTextView: AppCompatTextView {
         attrs,
         defStyleAttr
     )
+
+    init {
+        movementMethod = LinkMovementMethod.getInstance() // enable the click action
+        highlightColor = Color.TRANSPARENT // set background when clicked to transparent
+    }
+
+    private class ReadMoreClickableSpan(val context: Context): ClickableSpan() {
+        override fun onClick(widget: View) {
+            Toast.makeText(context, "clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun setText(text: CharSequence?, type: BufferType?) {
+        super.setText(getDisplayText(text), type)
+    }
+
+    private fun getDisplayText(text: CharSequence?) :CharSequence {
+        return SpannableStringBuilder(text).append("...").append("read more").apply {
+            setSpan(ReadMoreClickableSpan(context), this.length - 9, this.length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        }
+    }
+
 
 
 }
